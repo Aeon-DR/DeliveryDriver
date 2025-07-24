4d2ee25adddcb97c5132768601393eb25966aca1
Aeon-DR <dmitry.ryazantsev93@gmail.com>
2025-07-24 09:52:35 +0300

    Improve game layout

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,6 +4,7 @@ public class CarController : MonoBehaviour
 {
     [SerializeField] private float _turnSpeed = 250f;
     [SerializeField] private float _moveSpeed = 18f;
+    [SerializeField] private float _boostSpeed = 1f;
 
     private void Update()
     {
@@ -12,5 +13,10 @@ public class CarController : MonoBehaviour
 
         transform.Translate(Vector3.up * Time.deltaTime * _moveSpeed * moveAmount);
         transform.Rotate(Vector3.back, Time.deltaTime * _turnSpeed * steerAmount * moveAmount);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * (_moveSpeed + _boostSpeed) * moveAmount);
+        }
     }
 }
